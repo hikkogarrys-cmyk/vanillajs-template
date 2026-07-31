e2227c7f9fe76f1c4afe94a90807fd4cc17a90f6
hikkogarrys-cmyk <hikkogarrys@gmail.com>
2026-07-31 15:08:09 +0300

    Delete dist directory

--- dist/js/initTonConnect.js
+++
@@ -1,7 +0,0 @@
-function initTonConnectUI() {
-  const tonConnectUI = new window.TON_CONNECT_UI.TonConnectUI({
-    manifestUrl: new URL('/vanillajs-template/tonconnect-manifest.json', window.location.href).toString(),
-  });
-
-  return tonConnectUI;
-}
--- dist/js/utils.js
+++
@@ -1,9 +0,0 @@
-function filterChildren(children) {
-  return children.filter(function(c) {
-    return c !== undefined && c !== null && typeof c !== 'boolean';
-  });
-}
-
-function toArray(elem) {
-  return Array.isArray(elem) ? elem : [elem];
-}
